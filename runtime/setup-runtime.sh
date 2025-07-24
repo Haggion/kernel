406ec89f698de1cac5518e8e2e7238ebbb77ef13
Jahan Rashidi <hiimjahan@gmail.com>
2025-07-24 13:59:41 -0700

    Add to runtime

--- runtime/setup-runtime.sh
+++ runtime/setup-runtime.sh
@@ -1,3 +1,7 @@
+rm -f build/adainclude/*
+rm -f build/adalib/*
+rm -f obj/*
+
 for f in ./src/*.adb; do
 	riscv64-none-elf-gnatmake -c -gnatg -gnatA -nostdlib -nostartfiles -I./src -D ./obj -a "$f"
 done
---
+++ runtime/src/riscv64.ads
@@ -0,0 +1,10 @@
+-------------------------------------------------------------------------------
+-- RISCV64
+--
+-- Purpose:
+--  This package contains support code for the RISC-V 64bit system.
+-------------------------------------------------------------------------------
+
+package riscv64 is
+	pragma Preelaborate (riscv64);
+end riscv64;
