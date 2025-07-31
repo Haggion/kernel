29ecb07716f72750940ac66bc2f7dcfccebcca8b
Jahan Rashidi <hiimjahan@gmail.com>
2025-07-31 16:27:31 -0700

    Add some boilerplate for future stream support

--- src/runtime/io.adb
+++ src/runtime/io.adb
@@ -2,64 +2,99 @@ with Lines.Converter;
 with Ada.Unchecked_Conversion;
 
 package body IO is
-   procedure Put_Char (Ch : Character) is
+   procedure Put_Char (
+      Ch : Character;
+      S : Stream := UART_Stream
+   ) is
    begin
-      Put_Char (Character'Pos (Ch));
+      Put_Char (Character'Pos (Ch), S);
    end Put_Char;
 
-   procedure Put_String (Str : String;
-      End_With : Character := Character'Val (10)) is
+   procedure Put_Char (
+      Ch : Integer;
+      S : Stream := UART_Stream
+   ) is
+   begin
+      case S.Output is
+         when UART =>
+            UART_Put_Char (Ch);
+      end case;
+   end Put_Char;
+
+   procedure Put_String (
+      Str : String;
+      End_With : Character := Character'Val (10);
+      S : Stream := UART_Stream
+   ) is
    begin
       for Ch of Str loop
-         Put_Char (Ch);
+         Put_Char (Ch, S);
       end loop;
 
-      Put_Char (End_With);
+      Put_Char (End_With, S);
    end Put_String;
 
-   procedure Put_Line (Text : Line;
-      End_With : Character := Character'Val (10)) is
+   procedure Put_Line (
+      Text : Line;
+      End_With : Character := Character'Val (10);
+      S : Stream := UART_Stream
+   ) is
    begin
       for Ch of Text loop
          exit when Ch = Character'Val (0);
-         Put_Char (Ch);
+         Put_Char (Ch, S);
       end loop;
 
-      Put_Char (End_With);
+      Put_Char (End_With, S);
    end Put_Line;
 
-   procedure Put_Int (Int : Long_Integer) is
+   procedure Put_Int (
+      Int : Long_Integer;
+      S : Stream := UART_Stream
+   ) is
    begin
       Put_Line (
          Lines.Converter.Long_Int_To_Line (Int),
-         Character'Val (0)
+         Character'Val (0),
+         S
       );
    end Put_Int;
 
-   procedure New_Line is
+   procedure Put_C_Int (Int : Long_Integer) is
    begin
-      Put_Char (10);
+      Put_Int (Int);
+   end Put_C_Int;
+
+   procedure New_Line (S : Stream := UART_Stream) is
+   begin
+      Put_Char (10, S);
    end New_Line;
 
-   function Get_Char return Character is
+   function Get_Char (S : Stream := UART_Stream) return Character is
    begin
-      while Data_Ready = 0 loop
-         null;
-      end loop;
+      case S.Input is
+         when UART =>
+            while Data_Ready = 0 loop
+               null;
+            end loop;
 
-      return Last_Pressed;
+            return Last_Pressed;
+      end case;
    end Get_Char;
 
-   function Get_Line (Show_Typing : Boolean) return Line is
+   function Get_Line (
+      Show_Typing : Boolean;
+      S : Stream := UART_Stream
+   ) return Line is
       Input : Character;
       LineBuilder : Line := (others => Character'Val (0));
       Index : Line_Index := 1;
    begin
-      Input := Get_Char;
+      Input := Get_Char (S);
 
       while Character'Pos (Input) /= 13 loop
          if Show_Typing then
-            Put_Char (Input);
+            Put_Char (Input, S);
          end if;
 
          if Character'Pos (Input) = 127 then
@@ -68,7 +103,7 @@ package body IO is
                LineBuilder (Index) := Character'Val (0);
 
                if Show_Typing then
-                  Backspace;
+                  Backspace (S);
                end if;
             end if;
          else
@@ -76,18 +111,18 @@ package body IO is
             Index := Index + 1;
          end if;
 
-         Input := Get_Char;
+         Input := Get_Char (S);
       end loop;
 
       return LineBuilder;
    end Get_Line;
 
-   procedure Backspace is
+   procedure Backspace (S : Stream := UART_Stream) is
       Backspace_Char : constant Integer := 8;
    begin
-      Put_Char (Backspace_Char);
-      Put_Char (' ');
-      Put_Char (Backspace_Char);
+      Put_Char (Backspace_Char, S);
+      Put_Char (' ', S);
+      Put_Char (Backspace_Char, S);
    end Backspace;
 
    procedure Put_C_String (Text : Line) is
@@ -95,10 +130,13 @@ package body IO is
       Put_Line (Text, Character'Val (0));
    end Put_C_String;
 
-   procedure Put_Address (Address : System.Address) is
+   procedure Put_Address (
+      Address : System.Address;
+      S : Stream := UART_Stream
+   ) is
       function Adr_To_Int is new
          Ada.Unchecked_Conversion (System.Address, Long_Integer);
    begin
-      Put_Int (Adr_To_Int (Address));
+      Put_Int (Adr_To_Int (Address), S);
    end Put_Address;
 end IO;
--- src/runtime/io.ads
+++ src/runtime/io.ads
@@ -2,23 +2,55 @@ with Lines; use Lines;
 with System;
 
 package IO is
-   procedure Put_Char (Ch : Integer);
-   pragma Import (C, Put_Char, "putchar");
+   type Channel is (
+      UART
+   );
 
-   procedure Put_Char (Ch : Character);
-   procedure Put_String (Str : String;
-      End_With : Character := Character'Val (10));
-   procedure Put_Line (Text : Line;
-      End_With : Character := Character'Val (10));
-   procedure Put_Int (Int : Long_Integer);
-   pragma Export (C, Put_Int, "_put_int");
+   type Stream is record
+      Input : Channel;
+      Output : Channel;
+   end record;
 
-   procedure Put_Address (Address : System.Address);
+   UART_Stream : constant Stream := (UART, UART);
 
-   procedure New_Line;
+   procedure UART_Put_Char (Ch : Integer);
+   pragma Import (C, UART_Put_Char, "putchar");
 
-   function Get_Char return Character;
-   function Get_Line (Show_Typing : Boolean) return Line;
+   procedure Put_Char (
+      Ch : Integer;
+      S : Stream := UART_Stream
+   );
+   procedure Put_Char (
+      Ch : Character;
+      S : Stream := UART_Stream
+   );
+   procedure Put_String (
+      Str : String;
+      End_With : Character := Character'Val (10);
+      S : Stream := UART_Stream
+   );
+   procedure Put_Line (
+      Text : Line;
+      End_With : Character := Character'Val (10);
+      S : Stream := UART_Stream
+   );
+   procedure Put_Int (
+      Int : Long_Integer;
+      S : Stream := UART_Stream
+   );
+
+   procedure Put_Address (
+      Address : System.Address;
+      S : Stream := UART_Stream
+   );
+
+   procedure New_Line (S : Stream := UART_Stream);
+
+   function Get_Char (S : Stream := UART_Stream) return Character;
+   function Get_Line (
+      Show_Typing : Boolean;
+      S : Stream := UART_Stream
+   ) return Line;
 
 private
    function Last_Pressed return Character;
@@ -30,5 +62,8 @@ private
    procedure Put_C_String (Text : Line);
    pragma Export (C, Put_C_String, "_put_cstring");
 
-   procedure Backspace;
+   procedure Put_C_Int (Int : Long_Integer);
+   pragma Export (C, Put_C_Int, "_put_int");
+
+   procedure Backspace (S : Stream := UART_Stream);
 end IO;
