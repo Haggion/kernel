911d6ea7d7bf78cf9f79400323e96a70070830c1
Jahan Rashidi <hiimjahan@gmail.com>
2025-08-08 13:45:37 -0700

    Add options after catching exception in last chance handler

--- src/runtime/errors/last_chance_handler.adb
+++ src/runtime/errors/last_chance_handler.adb
@@ -3,12 +3,15 @@ with IO; use IO;
 with Ada.Unchecked_Conversion;
 with System.Unsigned_Types; use System.Unsigned_Types;
 with Lines.Converter; use Lines.Converter;
+with Driver_Handler;
 
 procedure Last_Chance_Handler
    (Source_Location : System.Address; Line : Integer) is
    function Adr_To_Int is new
       Ada.Unchecked_Conversion (System.Address, Long_Long_Unsigned);
 begin
+   New_Line;
+   
    String_Throw ("Some issue on line ", "last_chance_handler.adb");
    Put_Int (
       Long_Integer (Line)
@@ -21,7 +24,24 @@ begin
       )
    );
 
-   loop
-      null;
-   end loop;
+   Put_String ("Press (c) to continue, (s) to shutdown, or (r) to reboot");
+
+   declare
+      Input : Character;
+   begin
+      loop
+         Input := Get_Char;
+
+         case Input is
+            when 'c' =>
+               exit;
+            when 's' =>
+               Driver_Handler.Shutdown;
+            when 'r' =>
+               Driver_Handler.Reboot;
+            when others =>
+               null;
+         end case;
+      end loop;
+   end;
 end Last_Chance_Handler;
