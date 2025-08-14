5fb5cafc4ac74af311755a85651f129443117ae5
Jahan Rashidi <hiimjahan@gmail.com>
2025-08-14 16:20:57 -0700

    Change expected partition name to BOOT
    UEFI was a misnomer.

--- scripts/put_bin.sh
+++ scripts/put_bin.sh
@@ -2,5 +2,5 @@
 gmake clean TARGET=starfive bin || exit 1
 diskutil mount $1 || exit 1
 # misnominer
-cp -f build/kernel.bin /Volumes/UEFI/boot/kernel.bin
+cp -f build/kernel.bin /Volumes/BOOT/boot/kernel.bin
 diskutil unmount $1
